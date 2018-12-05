77c14b0cdfebd667249eaffa4484ac47a8b487b9
Elisey <elisey08@gmail.com>
2018-12-05 23:45:46 +0400

    modifications according to requirements

--- homework-2.1.py
+++ homework-2.1.py
@@ -1,24 +1,25 @@
-def get_components():
-    file_name = input('Введите имя файла для чтения рецептов: ')
+from pprint import pprint
+
+def get_cook_book(file_name):
     cook_book = {}
     dish_list = []
-    components_dict = {'ingridient_name': None, 'quantity': None, 'measure': None}
     with open(file_name) as f:
         for line in f:
             dish_name = line.strip()
             quantity_components = int(f.readline().strip())
-            for component in range(1, quantity_components + 1):
+            for component in range(quantity_components):
+                components_dict = {'ingridient_name': None, 'quantity': None, 'measure': None}
                 component = f.readline().strip().split('|')
-                components_dict['ingridient_name'] =  component[0]
-                components_dict['quantity'] = component[1]
-                components_dict['measure'] = component[2]
-                dish_list.append(components_dict.copy())
+                components_dict['ingridient_name'] =  component[0].strip()
+                components_dict['quantity'] = int(component[1].strip())
+                components_dict['measure'] = component[2].strip()
+                dish_list.append(components_dict)
             f.readline()
             cook_book[dish_name] = dish_list
+            dish_list = []
     return cook_book
 
-def get_shop_list_by_dishes(dishes, person_count):
-    cook_book = dishes
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
     shop_list = {}
     for dish in dishes:
         if dish in cook_book.keys():
@@ -27,6 +28,10 @@ def get_shop_list_by_dishes(dishes, person_count):
                 vol_ingridient = i.get('quantity')
                 measure = i.get('measure')
                 shop_list[name_ingridient] = {measure: vol_ingridient * person_count}
-    return print(shop_list)
+    return pprint(shop_list)
 
-get_shop_list_by_dishes(get_components(), 2)
+
+if __name__ == '__main__':
+    file_name = input('Введите имя файла для чтения рецептов: ')
+    cook_book = get_cook_book(file_name)
+    get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)

6d97bf38478b4f31e44a73d1490c56666d2943d9
Elisey <elisey08@gmail.com>
2018-12-11 22:33:18 +0400

    modif

--- homework-3.2.py
+++ homework-3.2.py
@@ -6,17 +6,14 @@ URL_API = 'https://api.vk.com/method/'
 
 url_get_token = 'https://oauth.vk.com/authorize?client_id=6773586&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52'
 
-TOKEN = 'ebd19367b930c8adc72dcb642a4545810d4fd7b40d830a5644ee8308a8413f044f7fe46481694f9dbb0cc'
-
-def find_total(first, second):
-    return list(set(first) & set(second))
+TOKEN = 'ed75f48dd8de0ddeb8f5875a7ecb970b101f80bd689130e67b313e29b97d53be91b7b0f5b185511704414'
 
 class User():
 
     def __init__(self, user_id):
         self.user_id = user_id
 
-    def search_matual_friends(self, user_id=None):
+    def get_friends_list(self, user_id=None):
         params = {
             'access_token': TOKEN,
             'v': '5.92',
@@ -26,14 +23,19 @@ class User():
         friends = resp.json()
         return friends['response']['items']
 
+    def find_total(self, first, second):
+        return list(set(first) & set(second))
+
     def __and__(self, other):
-        user_1 = self.search_matual_friends()
-        user_2 = other.search_matual_friends()
-        mutual_friends = find_total(user_1, user_2)
+        user_1 = self.get_friends_list()
+        user_2 = other.get_friends_list()
+        mutual_friends = self.find_total(user_1, user_2)
         return mutual_friends
 
     def __str__(self):
         return f'https://vk.com/id{self.user_id}'
+
+
 
 
 user_1 = User(97333658)
